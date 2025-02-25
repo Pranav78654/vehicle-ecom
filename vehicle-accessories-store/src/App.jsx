be9d2794f30c3d3a37609c1dacdb8e3862d3cda8
Raghav <raghav2004kaushal@gmail.com>
2025-02-25 12:16:51 +0530

    created card component and lower navbar component

--- vehicle-accessories-store/src/App.jsx
+++ vehicle-accessories-store/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
 import './App.css'
-
+import Card from './components/card'
+import LowerNavbar from './components/LowerNavbar'
 export default function App() {
   return (
     <>
     <Navbar/>
+    <LowerNavbar/>
+    <Card/>
     </>
   );
 }
