a985f396b8344a6b9b380e482f965e0848c24f6f
Divesh Gupta <divesh291.be22@chitkara.edu.in>
2025-03-04 12:12:26 +0530

    updated the app.jsx file

--- vehicle-accessories-store/src/App.jsx
+++ vehicle-accessories-store/src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
 import "./components/Signin"
-import Navbar from './components/Navbar'
+import UpperNavbar from './components/UpperNavbar'
 import './App.css'
-import Signin from './components/Signin'
+// import Signin from './components/Signin'
+import Footer from './components/Footer'
 
 export default function App() {
   return (
-    <>
-    <Navbar/>
-    {/* <Signin/> */}
-    
+      <>
+    <UpperNavbar/>
+    {/* <Signin/> */}.
+    <Footer/>
     
     </>
   );
