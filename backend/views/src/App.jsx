af5c36eb140a61fe3d36cf2280f40dd5370d14c1
Divesh Gupta <divesh291.be22@chitkara.edu.in>
2025-03-11 12:49:29 +0530

    added react router dom

--- backend/views/src/App.jsx
+++ backend/views/src/App.jsx
@@ -1,30 +1,41 @@
-import { useState } from 'react'
-import "./components/Signin"
-import UpperNavbar from './components/UpperNavbar'
-import './App.css'
-import Card from './components/card'
-import LowerNavbar from './components/LowerNavbar'
-import Signin from './components/Signin'
-import VehicleSelection from './components/VehicleSelection'
-// import Signin from './components/Signin'
-import Footer from './components/Footer'
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import Home from "./pages/Home";
+import Auth from "./pages/Auth";
+import UpperNavbar from "./components/UpperNavbar";
+import LowerNavbar from "./components/LowerNavbar";
+import Footer from "./components/Footer";
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/*" element={<MainLayout />} />
+        <Route path="/signin" element={<Auth />} /> {/* Signin page without Navbar/Footer */}
+      </Routes>
+    </Router>
+  );
+}
 
-export default function App() {
+function MainLayout() {
+  const location = useLocation();
+
+  // Define routes where Navbar and Footer should NOT be displayed
+  const noNavbarFooterRoutes = ["/signin"];
+
   return (
     <>
-    {/* <Navbar/> */}
-    <UpperNavbar/>
-    <LowerNavbar/>
-    <VehicleSelection/>
-    <LowerNavbar/>
-    <Card/>
-    {/* <Signin/> */}
-    
-      
-    {/* <Signin/> */}.
-    <Footer/>
-    
+      {/* Show Navbar only if the current route is NOT in noNavbarFooterRoutes */}
+      {!noNavbarFooterRoutes.includes(location.pathname) && <UpperNavbar />}
+      {!noNavbarFooterRoutes.includes(location.pathname) && <LowerNavbar />}
+      {/* Nested Routes for pages */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        
+      </Routes>
+
+      {/* Show Footer only if the current route is NOT in noNavbarFooterRoutes */}
+      {!noNavbarFooterRoutes.includes(location.pathname) && <Footer />}
     </>
   );
 }
 
+export default App;
---
+++ backend/views/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import '../App.css'
+import Card from '../components/Card'
+import VehicleSelection from '../components/VehicleSelection'
+export default function Home() {
+  return (
+    <>
+    
+    <VehicleSelection/>
+    <Card/>
+    </>
+  );
+}
+
