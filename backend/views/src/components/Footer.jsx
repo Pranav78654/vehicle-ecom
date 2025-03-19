9ad9c7078e1542743f2319513181767da4adbb91
Pb78654 <pb78654@gmail.com>
2025-03-19 15:00:19 +0530

    remove unwanted images and adjusted the css for Footer

--- backend/views/src/components/Footer.jsx
+++ backend/views/src/components/Footer.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="py-8 text-white" style={{ backgroundColor: "oklch(0.278 0.033 256.848)" }}>
+    <footer className="py-8 text-white bg-[#2C2231]/30">
+
       <div className="container mx-auto text-center">
         <div className="flex justify-center space-x-8 mb-4">
           {["About", "Blog", "Jobs", "Press", "Accessibility", "Partners"].map((link, index) => (
