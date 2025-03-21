f6b5d59539bf0671d18632295c30036ed4759881
Divesh Gupta <divesh291.be22@chitkara.edu.in>
2025-03-21 10:48:20 +0530

    completed the carousel for the home page

--- backend/views/src/pages/Home.jsx
+++ backend/views/src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import '../App.css'
 import Card from '../components/Card'
 import VehicleSelection from '../components/VehicleSelection'
+import CarCarousel from '../components/carousel';
 export default function Home() {
   return (
     <>
-    
+    <CarCarousel/>
     <VehicleSelection/>
     <Card/>
     </>
