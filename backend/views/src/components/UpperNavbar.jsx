e60842ab61fae68cc030d04223e3327d01ec790d
Divesh Gupta <divesh291.be22@chitkara.edu.in>
2025-04-12 19:45:11 +0530

    bug solved

--- backend/views/src/components/UpperNavbar.jsx
+++ backend/views/src/components/UpperNavbar.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser, faSearch, faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import { motion } from "framer-motion";
