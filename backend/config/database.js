066deeb44988acbd60ba319e40a32b82add690a2
Pb78654 <pb78654@gmail.com>
2025-10-26 13:02:13 +0530

    database connect to aiven mysql

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,22 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT,
-  logging: false, 
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // <- Allow self-signed certificate
+      }
+    }
+  }
+);
 
 module.exports = sequelize;
